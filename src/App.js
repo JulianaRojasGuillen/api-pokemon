feef097c4294e1396380728d9224c43f12226d9a
JulianaRojasGuillen <juliana.rojas@pucp.pe>
2022-04-04 11:59:23 -0500

    api-pokemon core terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const[seDioClickEnBuscar, setSeDioClickEnBuscar] = useState(false);
+  const[listaPokemones, setListaPokemones] = useState([]);
+  
+  const buscarNombrePokemon = (event) => {
+    event.preventDefault();
+    setSeDioClickEnBuscar(true);
+  }
+  
+  useEffect( () => {
+    axios.get( `https://pokeapi.co/api/v2/pokemon/?limit=807`)
+    .then ( respuesta => {
+      setListaPokemones( (listaPrev) => respuesta.data.results)
+    })
+    .catch( err => {
+      console.log(err);
+    });
+  },[buscarNombrePokemon]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={ (event) => buscarNombrePokemon(event)}>
+        <button type='submit' className='botonBuscar'>
+          Buscar Pokemon
+        </button>
+      </form>
+      <h1>
+        Pokemon List
+      </h1>
+      <div>
+        {
+          seDioClickEnBuscar?
+          listaPokemones.map( (pokemon, indice) => {
+            return(
+              <li key={indice}>{pokemon.name}</li>
+            );
+          })
+          :"Descubre los 807 pokemon dando click en el botón Buscar Pokemon"
+        }
+      </div>
     </div>
   );
 }
